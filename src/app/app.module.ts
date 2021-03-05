156ebe13ba00a836bcbef81d60793db8f0155da6
watcharin <twatcharin90@gmail.com>
2021-03-05 14:49:11 +0700

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignupComponent } from './signup/signup.component';
+import { SearchComponent } from './search/search.component';
+import { ContentNeedComponent } from './content-need/content-need.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderMenuComponent,
+    FooterComponent,
+    SignupComponent,
+    SearchComponent,
+    ContentNeedComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/content-need/content-need.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-need',
+  templateUrl: './content-need.component.html',
+  styleUrls: ['./content-need.component.css']
+})
+export class ContentNeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
